1e0ec3baf2aa5518a35a0ceac81e17074d84b8fd
anayavolapats <anay.avolapats@mail.ru>
2019-11-24 19:48:30 +0300

    All 3 tasks in 1

--- 2.2.2/src/Vector2D.java
+++ 2.2.2/src/Vector2D.java
@@ -1,20 +1,25 @@
 import java.util.Locale;
 
 public class Vector2D {
+    static int count;
     public double vX;
     public double vY;
     public Vector2D() {
         vX = 1;
         vY = 1;
+        count++;
     }
     public Vector2D(double vX, double vY) {
         this.vX = vX;
         this.vY = vY;
     }
     public double GetvX(){
+        count++;
         return vX;
+
     }
     public double GetvY(){
+        count++;
         return vY;
     }
     public void SetvX(double vX) {
@@ -44,5 +49,13 @@ public class Vector2D {
     public void scale(double scaleFactor){
         vY = vY * scaleFactor;
         vX = vX * scaleFactor;
+    }
+    public void normalized(){
+        double l = Math.sqrt(vX*vX + vY*vY);
+        this.vX = vX/l;
+        this.vY = vY/l;
+    }
+    public double dotProduct(Vector2D v){
+        return(v.vX*vX + v.vY*vY);
     }
 }
